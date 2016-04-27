efc928f10cfbc7000a727f54175431eb618436b1
jinu <jinu@192.168.0.17>
2016-04-27 10:45:49 +0900

    add

---
+++ LogStore/src/test/java/com/logStore/LogStoreApplicationTests.java
@@ -0,0 +1,18 @@
+package com.logStore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LogStoreApplication.class)
+@WebAppConfiguration
+public class LogStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
